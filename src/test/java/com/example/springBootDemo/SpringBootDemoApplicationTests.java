60b0b7dd8f6938eb432763d2179210720aa10c17
Andrzej <dekoderer@gmail.com>
2023-02-08 14:03:02 +0100

    Configured and added TestContainers to the project.

--- src/test/java/com/example/springBootDemo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/example/springBootDemo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,54 @@
 package com.example.springBootDemo;
 
+import com.example.springBootDemo.model.Employee;
+import com.example.springBootDemo.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Testcontainers
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = SpringBootDemoApplicationTests.DataSourceInitializer.class)
 class SpringBootDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    private static final PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:12.9-alpine");
 
+    @Autowired
+    private final EmployeeRepository employeeRepository;
+
+    SpringBootDemoApplicationTests(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Test
+    void saveToDb() {
+        var employee = new Employee(8L, "David");
+        employeeRepository.save(employee);
+        assertEquals(1, employeeRepository.findAll().size());
+    }
+
+    public static class DataSourceInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+                    applicationContext,
+                    "spring.datasource.url=" + database.getJdbcUrl(),
+                    "spring.datasource.username=" + database.getUsername(),
+                    "spring.datasource.password=" + database.getPassword()
+            );
+        }
+    }
 }
