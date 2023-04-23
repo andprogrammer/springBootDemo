1c4fa4bdc69a5ef8497669e6d380808e92c8b74a
Andrzej <andrzejggru@gmail.com>
2023-04-23 23:38:44 +0200

    Add swagger. Fix problem with hibernate validator once the app was starting.

--- src/main/java/com/example/springBootDemo/controller/EmployeeController.java
+++ src/main/java/com/example/springBootDemo/controller/EmployeeController.java
@@ -3,12 +3,15 @@ package com.example.springBootDemo.controller;
 import com.example.springBootDemo.dto.EmployeeRequest;
 import com.example.springBootDemo.dto.EmployeeResponse;
 import com.example.springBootDemo.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Employee Controller")
 @RestController
 @RequestMapping("/api/employee")
 @RequiredArgsConstructor
@@ -17,24 +20,28 @@ public class EmployeeController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Add new employee")
     public Long addEmployee(@RequestBody EmployeeRequest employeeRequest) {
         return employeeService.addEmployee(employeeRequest);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get all employees")
     public List<EmployeeResponse> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
 
     @GetMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Find employee by id")
     public EmployeeResponse findEmployeeById(@PathVariable(value = "id") Long employeeId) {
         return employeeService.getEmployee(employeeId);
     }
 
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Update employee")
     public EmployeeResponse updateEmployee(@PathVariable(value = "id") Long employeeId,
                                            @RequestBody EmployeeRequest employeeRequest) {
         return employeeService.updateEmployee(employeeId, employeeRequest);
@@ -42,6 +49,7 @@ public class EmployeeController {
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Delete employee")
     public void deleteEmployee(@PathVariable(value = "id") Long employeeId) {
         employeeService.deleteEmployee(employeeId);
     }
